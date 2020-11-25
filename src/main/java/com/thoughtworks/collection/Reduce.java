9055404d646b0924bc811fac67bc87a3d72d50ee
kenckl <klcheung8355@gmail.com>
2020-11-25 13:16:13 +0800

    [Stream API][Ken]Import Filter.java, MyMap.java and Reduce.java

--- src/main/java/com/thoughtworks/collection/Reduce.java
+++ src/main/java/com/thoughtworks/collection/Reduce.java
@@ -13,14 +13,17 @@ public class Reduce {
     }
 
     public double getAverage() {
-        throw new NotImplementedException();
+        return arrayList.stream().reduce(0,(num1,num2)->num1+num2).doubleValue()/arrayList.size();
+        //throw new NotImplementedException();
     }
 
     public int getMaxValue() {
-        throw new NotImplementedException();
+        return arrayList.stream().reduce(0, Integer::max);
+        //throw new NotImplementedException();
     }
 
     public int getLastOdd() {
-        throw new NotImplementedException();
+        return arrayList.stream().filter(integer -> integer%2 != 0).reduce(0, (first, second)->second);
+        //throw new NotImplementedException();
     }
 }
